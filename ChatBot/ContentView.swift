6302b5ab21a9da67a3b5e8751b0ebcf1711b4ce2
LiYanan2004 <37542129+LiYanan2004@users.noreply.github.com>
2023-03-02 17:46:12 +0800

    Initial Commit

---
+++ ChatBot/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ChatBot
+//
+//  Created by LiYanan2004 on 2023/3/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
